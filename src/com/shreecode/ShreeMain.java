3affe1157dc28c28e9cf9285b713a93efbee6795
stsh4g <46161319+stsh4g@users.noreply.github.com>
2019-01-09 22:43:03 +0530

    Update ShreeMain.java
    tesr

--- src/com/shreecode/ShreeMain.java
+++ src/com/shreecode/ShreeMain.java
@@ -3,10 +3,7 @@ package com.shreecode;
 public class ShreeMain {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello Mulul");
-=======
 		System.out.println("Shree And Satish");
->>>>>>> 00776179a8a7939cb02630611a3ec6b074e6b6fb
 	}
 }
